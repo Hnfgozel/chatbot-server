088409c4d3c7a56e5a4de8d7f78b9a70b5e8828c
Hanifi Gözel <gozel.hanifi@gmail.com>
2024-05-04 17:57:30 +0300

    fixes according to current openAI documentation.

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import OpenAI from 'openai'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import OpenAI from 'openai';
 
-dotenv.config()
+dotenv.config();
 
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', async (req, res) => {
     res.status(200).send({
         message: 'Hello from Server!'
-    })
-})
+    });
+});
 
 app.post('/', async (req, res) => {
     try {
-        const prompt = req.body.prompt;
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
+        const { chats } = req.body;
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "assistant", content: "You are a helpful assistant." }, ...chats],
             temperature: 0, // Higher values means the model will take more risks.
             max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
             top_p: 1, // alternative to sampling with temperature, called nucleus sampling
@@ -31,14 +32,16 @@ app.post('/', async (req, res) => {
             presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
         });
 
+        console.log(response.choices[0])
+
         res.status(200).send({
-            bot: response.data.choices[0].text
+            bot: response.choices[0]
         });
 
     } catch (error) {
-        console.error(error)
+        console.error(error);
         res.status(500).send(error || 'Something went wrong');
     }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
